dd5135e501b31761d6c3745536f6378f2a112ecf
1412284 <liem6396@gmail.com>
2020-07-06 16:42:44 +0700

    Fix name variable

--- main.go
+++ main.go
@@ -10,47 +10,43 @@ import (
 //MTRequest .........
 type MTRequest struct {
 	XMLName      xml.Name `xml:"MTRequest"`
-	Id           int      `xml:"id,attr"`
-	name         string   `xml:"name"`
-	userID       string   `xml:"user_id"`
-	message      string   `xml:"message"`
-	serviceID    string   `xml:"service_id"`
-	commandCode  string   `xml:"command_code"`
-	messageType  string   `xml:"message_type"`
-	requestID    string   `xml:"request_id"`
-	totalMessage string   `xml:"total_message"`
-	messageIndex string   `xml:"message_index"`
-	isMore       string   `xml:"is_more"`
-	contentType  string   `xml:"content_type"`
+	ID           int      `xml:"id,attr"`
+	Name         string   `xml:"name"`
+	UserID       string   `xml:"user_id"`
+	Message      string   `xml:"message"`
+	ServiceID    string   `xml:"service_id"`
+	CommandCode  string   `xml:"command_code"`
+	MessageType  string   `xml:"message_type"`
+	RequestID    string   `xml:"request_id"`
+	TotalMessage string   `xml:"total_message"`
+	MessageIndex string   `xml:"message_index"`
+	IsMore       string   `xml:"is_more"`
+	ContentType  string   `xml:"content_type"`
 }
 
-//KTResponse .......................
-type MTResponse struct {
-	status int `xml:status`
-}
+// //KTResponse .......................
+// type MTResponse struct {
+// 	Status int `xml:status`
+// }
 
 func main() {
 	var user string = "testapi"
 	var password string = "testapi22446688()"
 	var url string = " http://sms.8x77.vn:8077/mt-services/MTService"
 	reqMT := &MTRequest{
-		Id:           27,
-		name:         "MTResponse",
-		userID:       "84902866568",
-		message:      "abc",
-		serviceID:    "8077",
-		commandCode:  "CSKH",
-		messageType:  "0",
-		requestID:    "123456",
-		totalMessage: "1",
-		messageIndex: "1",
-		isMore:       "1",
-		contentType:  "1",
+		ID:           27,
+		Name:         "MTResponse",
+		UserID:       "84902866568",
+		Message:      "abc",
+		ServiceID:    "8077",
+		CommandCode:  "CSKH",
+		MessageType:  "0",
+		RequestID:    "123456",
+		TotalMessage: "1",
+		MessageIndex: "1",
+		IsMore:       "1",
+		ContentType:  "1",
 	}
-
-	out, _ := xml.MarshalIndent(reqMT, " ", "  ")
-	fmt.Println(string(out))
-	fmt.Println(xml.Header + string(out))
 	result := getResponseMT(user, password, url, reqMT)
 	fmt.Println(result)
 }
@@ -58,6 +54,8 @@ func main() {
 func getResponseMT(user string, password string, url string, reqMT *MTRequest) int {
 	client := &http.Client{}
 	body, _ := xml.MarshalIndent(reqMT, " ", "  ")
+	fmt.Println(string(body))
+	fmt.Println(xml.Header + string(body))
 	// build a new request, but not doing the POST yet
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(body)))
 	if err != nil {
